1b2b802a90f1ec4d4c476589ce066c8863e3fd67
Osadebe Chijioke <osadebec98@gmail.com>
2022-08-31 16:16:46 +0200

    Commiting so that I can get my appID

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
+const APP_ID = ""
+
+
+const uid = String(Math.floor(Math.random() * 10000)) //This defines the user id for the Agora Client
+const token = null //This defines the user token for the Agora Client, null because we won't be using tokens
+
 let localStream;
 let remoteStream;
 let peerConnection;
 
-let init = async () => {
-    peerConnection = new RTCPeerConnection()
+let constraints = {
+    video: true,
+    audio: false
 }
+
+let init = async () => {
+
+    let client = await AgoraRTM.createInstance()
+    console.log(client)
+
+    localStream = await navigator.mediaDevices.getUserMedia(constraints)
+    document.getElementById("user-1").srcObject = localStream
+}
+
+init()
