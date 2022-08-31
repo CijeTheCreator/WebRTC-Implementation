c7e12d84dd5234c125ffb0bf2b8001459d03d358
Osadebe Chijioke <osadebec98@gmail.com>
2022-08-31 16:26:02 +0200

    Initialized Agora

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const APP_ID = ""
+const APP_ID = "baaf8f04d086423f867f219de52be6bb"
 
 
 const uid = String(Math.floor(Math.random() * 10000)) //This defines the user id for the Agora Client
@@ -8,6 +8,9 @@ let localStream;
 let remoteStream;
 let peerConnection;
 
+let channel;
+let client;
+
 let constraints = {
     video: true,
     audio: false
@@ -15,11 +18,20 @@ let constraints = {
 
 let init = async () => {
 
-    let client = await AgoraRTM.createInstance()
-    console.log(client)
+    client = await AgoraRTM.createInstance(APP_ID)
+    await client.login({uid, token})
+
+    channel = client.createChannel("someChannelID")
+    channel.join()
+    channel.on("MemberJoined", handleUserJoined)
+    
 
     localStream = await navigator.mediaDevices.getUserMedia(constraints)
     document.getElementById("user-1").srcObject = localStream
+}
+
+let handleUserJoined = async (memberID) => {
+    console.log(memberID)
 }
 
 init()
